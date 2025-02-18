402d0eda99b70e9f54b2c2a584d6d7063d82caee
ardevpk <abdulrehmandev12@gmail.com>
2025-02-19 00:56:42 +0500

    hotfix: for testing Cors

--- auth/app.py
+++ auth/app.py
@@ -37,10 +37,11 @@ app.add_middleware(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=settings.allow_origin_regex,
+    # allow_origin_regex=settings.allow_origin_regex,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origins=["*"],
 )
 
 
